0297a30238870a6b4eb7521ad2ce34963c7bd834
showzeng <kingstageshow@gmail.com>
2019-02-03 11:08:28 +0800

    (optimizing) rename link_target.js to post_content_init.js and add init of zooming

---
+++ assets/js/post_content_init.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+  // Make article links open in new tab by default
+  var anchors = document.getElementById('post-content').getElementsByTagName('a');
+  for (var i=0; i<anchors.length; i++){
+    anchors[i].setAttribute('target', '_blank');
+  }
+
+  // Listen to images after DOM content is fully loaded
+  var images = document.getElementById('post-content').getElementsByTagName('img');
+  var zooming = new Zooming();
+  for (var i=0; i<images.length; i++){
+    zooming.listen(images[i]);
+  }
+}
